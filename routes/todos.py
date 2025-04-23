04386ec519bb93b51c342d3d7df8b111330530d2
dhiabj <dhia.bejaoui.db@gmail.com>
2025-04-23 20:38:47 +0100

    implement crud

--- routes/todos.py
+++ routes/todos.py
@@ -11,3 +11,19 @@ router = APIRouter()
 def get_todos():
     todos = list_todo(todo_collection.find())
     return todos
+
+
+@router.post("/")
+def post_todo(todo: Todo):
+    todo_collection.insert_one(dict(todo))
+
+
+@router.put("/{id}")
+def put_todo(id: str, todo: Todo):
+    todo_collection.find_one_and_update(
+        {"_id": ObjectId(id)}, {"$set": dict(todo)})
+
+
+@router.delete("/{id}")
+def delete_todo(id: str):
+    todo_collection.find_one_and_delete({"_id": ObjectId(id)})
